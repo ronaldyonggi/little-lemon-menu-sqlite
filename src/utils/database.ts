0f481150e292ca2b261d3b0c23c7154e2e2199c7
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-21 19:22:08 +0800

    add comment in implementation

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -65,4 +65,24 @@ export async function filterByQueryAndCategories(query, activeCategories) {
   return new Promise((resolve, reject) => {
     resolve(SECTION_LIST_MOCK_DATA);
   });
+  // db = await SQLite.openDatabaseAsync('little_lemon');
+
+  // if (query && activeCategories) {
+  //   return await db.getAllAsync(
+  //     'SELECT * FROM menuitems WHERE (category in ? and title like ?)',
+  //     [activeCategories, `%${query}%`]
+  //   );
+  // } else if (query) {
+  //   return await db.getAllAsync(
+  //     'SELECT * FROM menuitems WHERE title like ?',
+  //     `%${query}%`
+  //   );
+  // } else if (activeCategories) {
+  //   return await db.getAllAsync(
+  //     `SELECT * FROM menuitems WHERE category in ?)`,
+  //     activeCategories
+  //   );
+  // } else {
+  //   return await db.getAllAsync(`SELECT * FROM menuitems`);
+  // }
 }
